cbb93838a0a830082377ff2571f7458f3705064f
jeanmarc LECUL <togashifr@outlook.fr>
2024-10-07 23:48:00 +0200

    add collisions and brick life

--- settings.py
+++ settings.py
@@ -3,12 +3,13 @@ def init():
     WIDTH, HEIGHT = 1024, 600
 
     # Colors
-    global WHITE, BLACK, RED, BLUE, GREEN, YELLOW, GREEN_MID, GREEN_LOW
+    global WHITE, BLACK, RED, BLUE, GREEN, YELLOW, CYAN, GREEN_MID, GREEN_LOW
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     RED = (255, 0, 0)
     BLUE = (0, 0, 255)
-    YELLOW = (0, 255, 255)
+    CYAN = (0, 255, 255)
+    YELLOW = (255, 255, 0)
     GREEN = (0, 255, 0)
-    GREEN_MID = (0, 200, 0)
-    GREEN_LOW = (0, 150, 0)
+    GREEN_MID = (0, 155, 0)
+    GREEN_LOW = (0, 55, 0)
